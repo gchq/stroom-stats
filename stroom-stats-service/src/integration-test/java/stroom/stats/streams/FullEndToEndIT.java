454868fd164c420ef84cf71373812c5e88857cb2
jc064522 <jc064522@gmail.com>
2017-03-24 10:59:17 +0000

    Refactor for shorter methods
    I was having trouble following the code or knowing what bit depended on what other objects.

--- stroom-stats-service/src/integration-test/java/stroom/stats/streams/FullEndToEndIT.java
+++ stroom-stats-service/src/integration-test/java/stroom/stats/streams/FullEndToEndIT.java
@@ -63,22 +63,110 @@ public class FullEndToEndIT extends AbstractAppIT {
     private SessionFactory sessionFactory = injector.getInstance(SessionFactory.class);
     private StatisticConfigurationEntityMarshaller statisticConfigurationEntityMarshaller = injector.getInstance(StatisticConfigurationEntityMarshaller.class);
     private StroomPropertyService stroomPropertyService = injector.getInstance(StroomPropertyService.class);
-    private StatisticsMarshaller statisticsMarshaller = injector.getInstance(StatisticsMarshaller.class);
 
-    private String tagEnv = "environment";
-    private String valEnvOps = "OPS";
-    private String valEnvDev = "DEV";
-    private String tagSystem = "system";
-    private String valSystemABC = "SystemABC";
-    private String valSystemXYZ = "SystemXYZ";
+    private final static String TAG_ENV = "environment";
+    private final static String VAL_ENV_OPS = "OPS";
+    private final static String VAL_ENV_DEV = "DEV";
+    private final static String TAG_SYSTEM = "system";
+    private final static String VAL_SYSTEM_ABC = "SystemABC";
+    private final static String VAL_SYSTEM_XZY = "SystemXYZ";
+
+    private final static List<Tuple2<String, String>> VALUE_PAIRS = Arrays.asList(
+            new Tuple2<>(VAL_ENV_OPS, VAL_SYSTEM_ABC),
+            new Tuple2<>(VAL_ENV_OPS, VAL_SYSTEM_XZY),
+            new Tuple2<>(VAL_ENV_DEV, VAL_SYSTEM_ABC),
+            new Tuple2<>(VAL_ENV_DEV, VAL_SYSTEM_XZY));
+    private final static int STATS_IN_BATCH = 10;
+    private final static int TIME_DELTA_MS = 250;
+    private final static long MAX_ITERATIONS = 8;
+
 
     @Test
     public void testAllTypesAndIntervals() {
+        //start at the beginning of today
+        ZonedDateTime startTime = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS);
+        LOGGER.info("Start time is {}", startTime);
 
+        configure(stroomPropertyService);
+        Map<Tuple2<StatisticType, EventStoreTimeIntervalEnum>, String> statNameMap =
+                buildStatNameMap(stroomPropertyService, sessionFactory, statisticConfigurationEntityMarshaller);
+
+        Map<String, List<Statistics>> statistics = buildStatistics(
+                statNameMap,
+                stroomPropertyService.getPropertyOrThrow(KafkaStreamService.PROP_KEY_STATISTIC_EVENTS_TOPIC_PREFIX),
+                startTime);
+
+
+        sendStatistics(
+                buildKafkaProducer(stroomPropertyService),
+                statistics,
+                injector.getInstance(StatisticsMarshaller.class));
+
+        ThreadUtil.sleep(60_000);
+
+    }
+
+    private static Map<String, List<Statistics>> buildStatistics(Map<Tuple2<StatisticType, EventStoreTimeIntervalEnum>, String> statNameMap, String topicPrefix, ZonedDateTime startTime) {
+        Map<String, List<Statistics>> topicToStatistics = new HashMap<>();
+        final AtomicLong counter = new AtomicLong(0);
+        List<Statistics.Statistic> statList = new ArrayList<>();
+
+        statNameMap.entrySet().forEach(entry -> {
+            String statName = entry.getValue();
+            StatisticType statisticType = entry.getKey()._1();
+            EventStoreTimeIntervalEnum interval = entry.getKey()._2();
+
+            LOGGER.info("Processing {} - {}", statisticType, interval);
+
+            String topic = TopicNameFactory.getStatisticTypedName(topicPrefix, statisticType);
+
+
+            while (counter.get() < MAX_ITERATIONS) {
+                ZonedDateTime time = startTime.plus(TIME_DELTA_MS * counter.get(), ChronoUnit.MILLIS);
+
+                Tuple2<String, String> valuePair = VALUE_PAIRS.get((int) (counter.get() % VALUE_PAIRS.size()));
+
+                TagType tagTypeEnv = StatisticsHelper.buildTagType(TAG_ENV, valuePair._1());
+                TagType tagTypeSystem = StatisticsHelper.buildTagType(TAG_SYSTEM, valuePair._2());
+                Statistics.Statistic statistic;
+                if (statisticType.equals(StatisticType.COUNT)) {
+                    statistic = StatisticsHelper.buildCountStatistic(statName, time, 10L, tagTypeEnv, tagTypeSystem);
+                } else {
+                    statistic = StatisticsHelper.buildValueStatistic(statName, time, 0.5, tagTypeEnv, tagTypeSystem);
+                }
+
+                statList.add(statistic);
+
+                if (counter.get() != 0 && (counter.get() % STATS_IN_BATCH == 0 || counter.get() == (MAX_ITERATIONS - 1))) {
+                    Statistics statistics = StatisticsHelper.buildStatistics(statList.toArray(new Statistics.Statistic[statList.size()]));
+                    statList.clear();
+                    if(topicToStatistics.containsKey(topic)) {
+                        topicToStatistics.get(topic).add(statistics);
+                    }
+                    else{
+                        topicToStatistics.put(topic, Arrays.asList(statistics));
+                    }
+                }
+                counter.incrementAndGet();
+            }
+
+            //reset for the next round
+            counter.set(0);
+        });
+
+        return topicToStatistics;
+    }
+
+
+    private static void configure(StroomPropertyService stroomPropertyService){
         //set small batch size and flush interval so we don't have to wait ages for data to come through
         stroomPropertyService.setProperty(StatisticsAggregationProcessor.PROP_KEY_AGGREGATOR_MIN_BATCH_SIZE, 10);
         stroomPropertyService.setProperty(StatisticsAggregationProcessor.PROP_KEY_AGGREGATOR_MAX_FLUSH_INTERVAL_MS, 500);
+    }
 
+    private static Map<Tuple2<StatisticType, EventStoreTimeIntervalEnum>, String> buildStatNameMap(StroomPropertyService stroomPropertyService,
+                                         SessionFactory sessionFactory,
+                                         StatisticConfigurationEntityMarshaller statisticConfigurationEntityMarshaller){
         Map<Tuple2<StatisticType, EventStoreTimeIntervalEnum>, String> statNameMap = new HashMap<>();
 
         //build a map of all the stat names and add them as StatConfig entities
@@ -93,7 +181,7 @@ public class FullEndToEndIT extends AbstractAppIT {
                 //TODO temporary filter, remove
                 if (interval.equals(EventStoreTimeIntervalEnum.SECOND)) {
 //                if (interval.equals(EventStoreTimeIntervalEnum.SECOND) && statisticType.equals(StatisticType.COUNT)) {
-                    String statNameStr = this.getClass().getName() + "-test-" + Instant.now().toString() + "-" + statisticType + "-" + interval;
+                    String statNameStr = "FullEndToEndIT-test-" + Instant.now().toString() + "-" + statisticType + "-" + interval;
                     LOGGER.info("Creating stat name : {}", statNameStr);
 
                     statNameMap.put(new Tuple2(statisticType, interval), statNameStr);
@@ -102,7 +190,7 @@ public class FullEndToEndIT extends AbstractAppIT {
                             statisticType,
                             interval.columnInterval(),
                             StatisticRollUpType.ALL)
-                            .addFields(tagEnv, tagSystem)
+                            .addFields(TAG_ENV, TAG_SYSTEM)
                             .build();
 
                     StatisticConfigurationEntityHelper.addStatConfig(
@@ -113,21 +201,10 @@ public class FullEndToEndIT extends AbstractAppIT {
             });
         });
 
-        //start at the beginning of today
-        ZonedDateTime startTime = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS);
-        LOGGER.info("Start time is {}", startTime);
-        int statsInBatch = 10;
-        int timeDeltaMs = 250;
-        long maxIterations = 8;
-        final AtomicLong counter = new AtomicLong(0);
-        List<Tuple2<String, String>> valuePairs = new ArrayList<>();
-        valuePairs.add(new Tuple2<>(valEnvOps, valSystemABC));
-        valuePairs.add(new Tuple2<>(valEnvOps, valSystemXYZ));
-        valuePairs.add(new Tuple2<>(valEnvDev, valSystemABC));
-        valuePairs.add(new Tuple2<>(valEnvDev, valSystemXYZ));
+        return statNameMap;
+    }
 
-        List<Statistics.Statistic> statList = new ArrayList<>();
-
+    private static KafkaProducer<String, String> buildKafkaProducer(StroomPropertyService stroomPropertyService){
         Map<String, Object> producerProps = new HashMap<>();
         producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 stroomPropertyService.getPropertyOrThrow(KafkaStreamService.PROP_KEY_KAFKA_BOOTSTRAP_SERVERS));
@@ -140,68 +217,31 @@ public class FullEndToEndIT extends AbstractAppIT {
         Serde<String> stringSerde = Serdes.String();
 
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(producerProps, stringSerde.serializer(), stringSerde.serializer());
+        return kafkaProducer;
+    }
 
-        String topicPrefix = stroomPropertyService.getPropertyOrThrow(KafkaStreamService.PROP_KEY_STATISTIC_EVENTS_TOPIC_PREFIX);
+    private static void sendStatistics(
+            KafkaProducer<String, String> kafkaProducer,
+            Map<String, List<Statistics>> statisticTypeToStatistics,
+            StatisticsMarshaller statisticsMarshaller){
+        statisticTypeToStatistics.entrySet().stream().forEach(
+                entry ->entry.getValue().forEach(
+                        statistics -> {
+                            ProducerRecord<String, String> producerRecord = buildProducerRecord(entry.getKey(), statistics, statisticsMarshaller);
 
-        statNameMap.entrySet().forEach(entry -> {
-            String statName = entry.getValue();
-            StatisticType statisticType = entry.getKey()._1();
-            EventStoreTimeIntervalEnum interval = entry.getKey()._2();
+                            statistics.getStatistic().forEach(statistic ->
+                                    LOGGER.trace("Sending stat with name {}, count {} and value {}", statistic.getName(), statistic.getCount(), statistic.getValue())
+                            );
 
-            LOGGER.info("Processing {} - {}", statisticType, interval);
-
-            String topic = TopicNameFactory.getStatisticTypedName(topicPrefix, statisticType);
-
-
-            while (counter.get() < maxIterations) {
-                ZonedDateTime time = startTime.plus(timeDeltaMs * counter.get(), ChronoUnit.MILLIS);
-
-                Tuple2<String, String> valuePair = valuePairs.get((int) (counter.get() % valuePairs.size()));
-
-                TagType tagTypeEnv = StatisticsHelper.buildTagType(tagEnv, valuePair._1());
-                TagType tagTypeSystem = StatisticsHelper.buildTagType(tagSystem, valuePair._2());
-                Statistics.Statistic statistic;
-                if (statisticType.equals(StatisticType.COUNT)) {
-                    statistic = StatisticsHelper.buildCountStatistic(statName, time, 10L, tagTypeEnv, tagTypeSystem);
-                } else {
-                    statistic = StatisticsHelper.buildValueStatistic(statName, time, 0.5, tagTypeEnv, tagTypeSystem);
-                }
-
-                statList.add(statistic);
-
-                if (counter.get() != 0 && (counter.get() % statsInBatch == 0 || counter.get() == (maxIterations - 1))) {
-                    Statistics statistics = StatisticsHelper.buildStatistics(statList.toArray(new Statistics.Statistic[statList.size()]));
-                    statList.clear();
-                    sendStatistics(kafkaProducer, topic, statistics);
-                }
-                counter.incrementAndGet();
-            }
-
-            //reset for the next round
-            counter.set(0);
-        });
+                            LOGGER.trace(() -> String.format("Sending %s stat events to topic $s", statistics.getStatistic().size(), entry.getKey()));
+                            kafkaProducer.send(producerRecord);
+                        }));
 
         kafkaProducer.flush();
         kafkaProducer.close();
-
-        ThreadUtil.sleep(60_000);
-
     }
 
-
-    private void sendStatistics(KafkaProducer<String, String> kafkaProducer, String topic, Statistics statistics) {
-
-        ProducerRecord<String, String> producerRecord = buildProducerRecord(topic, statistics);
-
-        statistics.getStatistic().forEach(statistic ->
-                LOGGER.trace("Sending stat with name {}, count {} and value {}", statistic.getName(), statistic.getCount(), statistic.getValue())
-        );
-
-        LOGGER.trace(() -> String.format("Sending %s stat events to topic $s", statistics.getStatistic().size(), topic));
-        kafkaProducer.send(producerRecord);
-    }
-
-    private ProducerRecord<String, String> buildProducerRecord(String topic, Statistics statistics) {
+    private static ProducerRecord<String, String> buildProducerRecord(String topic, Statistics statistics, StatisticsMarshaller statisticsMarshaller) {
         String statName = statistics.getStatistic().get(0).getName();
         return new ProducerRecord<>(topic, statName, statisticsMarshaller.marshallXml(statistics));
     }
