e0de9f3538d96f3f2e7741042404fc584465ffdc
jc064522 <jc064522@gmail.com>
2017-03-30 15:03:53 +0100

    Add intersection method (WiP)

--- stroom-stats-service/src/integration-test/java/stroom/stats/correlation/Correlator.java
+++ stroom-stats-service/src/integration-test/java/stroom/stats/correlation/Correlator.java
@@ -3,10 +3,13 @@ package stroom.stats.correlation;
 import com.google.common.collect.ImmutableList;
 import stroom.query.api.Row;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Correlator {
 
@@ -24,8 +27,27 @@ public class Correlator {
                 .flatMap(entry -> entry.getValue().stream()) // We don't care about sets now, just about rows
                 .filter(row -> !set.contains(row)) // We only want stuff not in our main set
                 .collect(Collectors.toList());
-
         return complementOfSet;
+    }
+
+    public List<Row> intersection(SetName... setNames) {
+        List<ImmutableList<Row>> setsForIntersection = new ArrayList<>();
+        Stream.of(setNames).forEach(setName -> setsForIntersection.add(sets.get(setName)));
+
+        if(setsForIntersection.size() >= 2){
+//            intersection(se)
+        }
+        return null; //TODO
+    }
+
+    private Set<Row> intersection(Set<Row> intersectionAccumulator, List<Row>... remainingSets){
+        if(remainingSets.length >= 2) {
+            return intersectionAccumulator.stream().filter(remainingSets[0]::contains).collect(Collectors.toSet());
+        }
+        else{
+            return intersectionAccumulator;
+        }
+        //TODO recurse
     }
 
     public enum SetName {
