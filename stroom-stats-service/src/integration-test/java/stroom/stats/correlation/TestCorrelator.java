6730d317d644bed0ecc3198b5ead118757cb2b72
jc064522 <jc064522@gmail.com>
2017-03-31 08:59:05 +0100

    Simplify tests

--- stroom-stats-service/src/integration-test/java/stroom/stats/correlation/TestCorrelator.java
+++ stroom-stats-service/src/integration-test/java/stroom/stats/correlation/TestCorrelator.java
@@ -1,5 +1,6 @@
 package stroom.stats.correlation;
 
+import com.google.common.collect.Sets;
 import org.junit.Test;
 import stroom.query.api.Row;
 
@@ -19,19 +20,37 @@ public class TestCorrelator {
     private static final String SET_D = "D";
     private static final String SET_E = "E";
 
+    private static final Set<Row> A = Sets.newHashSet(
+        new Row("groupKey", Arrays.asList("user1", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user2", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user3", "door1"), 1));
+
+    private static final Set<Row> B = Sets.newHashSet(
+        new Row("groupKey", Arrays.asList("user1", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user2", "door1"), 1));
+
+    private static final Set<Row> C = Sets.newHashSet(
+        new Row("groupKey", Arrays.asList("user1", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user2", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user3", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user4", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user5", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user6", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user7", "door1"), 1));
+
+    private static final Set<Row> D = Sets.newHashSet(
+        new Row("groupKey", Arrays.asList("user1", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user2", "door1"), 1),
+        new Row("groupKey", Arrays.asList("user3", "door1"), 1));
+
+    private static final Set<Row> E = Sets.newHashSet(
+        new Row("groupKey", Arrays.asList("user2", "door1"), 1));
+
     @Test
     public void testComplement(){
-        Set<Row> a = new HashSet<>();
-        a.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        Set<Row> b = new HashSet<>();
-        b.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-
         List<Row> complementOfB = new Correlator()
-                .addSet(SET_A, a)
-                .addSet(SET_B, b)
+                .addSet(SET_A, A)
+                .addSet(SET_B, B)
                 .complement(SET_B);
 
         assertThat(complementOfB.size()).isEqualTo(1);
@@ -41,17 +60,9 @@ public class TestCorrelator {
 
     @Test
     public void test_intersection_with_2_sets(){
-        Set<Row> a = new HashSet<>();
-        a.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        Set<Row> b = new HashSet<>();
-        b.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-
         Set<Row> intersectionOfAandB = new Correlator()
-                .addSet(SET_A, a)
-                .addSet(SET_B, b)
+                .addSet(SET_A, A)
+                .addSet(SET_B, B)
                 .intersection(SET_A, SET_B);
         List<Row> intersection = new ArrayList<>(intersectionOfAandB);
 
@@ -62,38 +73,15 @@ public class TestCorrelator {
 
     @Test
     public void test_intersection_with_4_sets(){
-        Set<Row> a = new HashSet<>();
-        a.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        Set<Row> b = new HashSet<>();
-        b.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        Set<Row> c = new HashSet<>();
-        c.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user6", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user7", "door1"), 1));
-        Set<Row> d = new HashSet<>();
-        d.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
         Set<Row> intersectionOfAandB = new Correlator()
-                .addSet(SET_A, a)
-                .addSet(SET_B, b)
-                .addSet(SET_C, c)
-                .addSet(SET_D, d)
+                .addSet(SET_A, A)
+                .addSet(SET_B, B)
+                .addSet(SET_C, C)
+                .addSet(SET_D, D)
                 .intersection(SET_A, SET_B, SET_C, SET_D);
         List<Row> intersection = new ArrayList<>(intersectionOfAandB);
 
-        assertThat(intersection.size()).isEqualTo(3);
+        assertThat(intersection.size()).isEqualTo(2);
         assertThat(intersection.get(0).getValues().get(0)).isEqualTo("user1");
         assertThat(intersection.get(1).getValues().get(0)).isEqualTo("user2");
     }
@@ -101,86 +89,34 @@ public class TestCorrelator {
 
     @Test
     public void test_intersection_with_5_sets_small_intersection(){
-        Set<Row> a = new HashSet<>();
-        a.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        Set<Row> b = new HashSet<>();
-        b.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        Set<Row> c = new HashSet<>();
-        c.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user6", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user7", "door1"), 1));
-        Set<Row> d = new HashSet<>();
-        d.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        Set<Row> e = new HashSet<>();
-        e.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
         Set<Row> intersectionOfAandB = new Correlator()
-                .addSet(SET_A, a)
-                .addSet(SET_B, b)
-                .addSet(SET_C, c)
-                .addSet(SET_D, d)
-                .addSet(SET_E, e)
+                .addSet(SET_A, A)
+                .addSet(SET_B, B)
+                .addSet(SET_C, C)
+                .addSet(SET_D, D)
+                .addSet(SET_E, E)
                 .intersection(SET_A, SET_B, SET_C, SET_D, SET_E);
         List<Row> intersection = new ArrayList<>(intersectionOfAandB);
 
         assertThat(intersection.size()).isEqualTo(1);
-        assertThat(intersection.get(0).getValues().get(0)).isEqualTo("user3");
+        assertThat(intersection.get(0).getValues().get(0)).isEqualTo("user2");
     }
 
 
     @Test
     public void test_intersection_with_5_sets_intersect_fewer(){
-        Set<Row> a = new HashSet<>();
-        a.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        a.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        Set<Row> b = new HashSet<>();
-        b.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        b.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        Set<Row> c = new HashSet<>();
-        c.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user4", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user5", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user6", "door1"), 1));
-        c.add(new Row("groupKey", Arrays.asList("user7", "door1"), 1));
-        Set<Row> d = new HashSet<>();
-        d.add(new Row("groupKey", Arrays.asList("user1", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user2", "door1"), 1));
-        d.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
-        Set<Row> e = new HashSet<>();
-        e.add(new Row("groupKey", Arrays.asList("user3", "door1"), 1));
         Set<Row> intersectionOfAandB = new Correlator()
-                .addSet(SET_A, a)
-                .addSet(SET_B, b)
-                .addSet(SET_C, c)
-                .addSet(SET_D, d)
-                .addSet(SET_E, e)
-                .intersection(SET_B, SET_C);
+                .addSet(SET_A, A)
+                .addSet(SET_B, B)
+                .addSet(SET_C, C)
+                .addSet(SET_D, D)
+                .addSet(SET_E, E)
+                .intersection(SET_C, SET_D);
         List<Row> intersection = new ArrayList<>(intersectionOfAandB);
 
-        assertThat(intersection.size()).isEqualTo(5);
+        assertThat(intersection.size()).isEqualTo(3);
         assertThat(intersection.get(0).getValues().get(0)).isEqualTo("user1");
         assertThat(intersection.get(1).getValues().get(0)).isEqualTo("user2");
         assertThat(intersection.get(2).getValues().get(0)).isEqualTo("user3");
-        assertThat(intersection.get(3).getValues().get(0)).isEqualTo("user4");
-        assertThat(intersection.get(4).getValues().get(0)).isEqualTo("user5");
     }
 }
