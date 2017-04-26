bcf2f84389c70be656be9cd6a5241ae65ef8f73e
jc064522 <jc064522@gmail.com>
2017-04-26 06:47:23 +0100

    Disable MySQL (WiP)

--- .travis.install.sh
+++ .travis.install.sh
@@ -33,10 +33,10 @@ function sortItAllOut {
 sortItAllOut stroom
 
 # Set up the database - we need to set up a custom user otherwise we'll have trouble connecting in Travis CI
-mysql -e "CREATE DATABASE IF NOT EXISTS stroom;"
-mysql -e "CREATE USER 'stroomuser'@'localhost' IDENTIFIED BY 'stroompassword1';"
-mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'stroomuser'@'localhost';"
-mysql -e "FLUSH PRIVILEGES"
+#mysql -e "CREATE DATABASE IF NOT EXISTS stroom;"
+#mysql -e "CREATE USER 'stroomuser'@'localhost' IDENTIFIED BY 'stroompassword1';"
+#mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'stroomuser'@'localhost';"
+#mysql -e "FLUSH PRIVILEGES"
 
 # Install the urlDependencies plugin
 git clone https://github.com/gchq/urlDependencies-plugin.git
