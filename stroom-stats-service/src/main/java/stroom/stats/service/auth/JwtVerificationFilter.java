b128dd75767dd4b8667fc6df6d7d8002f48857b5
jc064522 <jc064522@gmail.com>
2017-09-26 06:42:36 +0100

    Add copyright headers

--- stroom-stats-service/src/main/java/stroom/stats/service/auth/JwtVerificationFilter.java
+++ stroom-stats-service/src/main/java/stroom/stats/service/auth/JwtVerificationFilter.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * This file is part of Stroom-Stats.
+ *
+ * Stroom-Stats is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Stroom-Stats is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stroom-Stats.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package stroom.stats.service.auth;
 
 import io.dropwizard.auth.AuthFilter;
 import io.dropwizard.auth.AuthenticationException;
-import io.dropwizard.auth.Authenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stroom.stats.service.config.Config;
