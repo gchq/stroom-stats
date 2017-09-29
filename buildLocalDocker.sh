36c74f4386ce852e76b905c2fdb2b03401d0ed45
at055612 <at055612@gmail.com>
2017-09-29 13:16:42 +0100

    Relocated Dockerfile, add after_success script

---
+++ buildLocalDocker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#run a build with no tests for speed
+./buildNoIntTests.sh
+
+#define the docker context as stroom-app/docker as this is where all the docker artefacts are, including the dockerfile
+docker build --tag=gchq/stroom-stats:v1.0.0-LOCAL-SNAPSHOT --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy ./docker/.
---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#set -e
+set -o errexit
+
+echo "doing something wrong now .."
+xxxxxxx
+
+echo "end"
+
+exit 0
