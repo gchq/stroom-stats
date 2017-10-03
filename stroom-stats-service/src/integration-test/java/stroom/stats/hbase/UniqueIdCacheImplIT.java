eb44b690a608cd1dfbaf1aeff315625e489087ed
at055612 <at055612@gmail.com>
2017-10-03 07:48:39 +0100

    Add UniqueIdCacheImpl

--- stroom-stats-service/src/integration-test/java/stroom/stats/hbase/UniqueIdCacheImplIT.java
+++ stroom-stats-service/src/integration-test/java/stroom/stats/hbase/UniqueIdCacheImplIT.java
@@ -1,4 +1,27 @@
 package stroom.stats.hbase;
 
-public class UniqueIdCacheImplIT {
+import com.google.inject.Injector;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import stroom.stats.AbstractAppIT;
+import stroom.stats.hbase.uid.UID;
+import stroom.stats.hbase.uid.UniqueIdCache;
+
+public class UniqueIdCacheImplIT extends AbstractAppIT {
+
+    private final Injector injector = getApp().getInjector();
+    private final UniqueIdCache uniqueIdCache = injector.getInstance(UniqueIdCache.class);
+
+    @Test
+    public void testGetOrCreateId() {
+
+        String uuid = "myUUID";
+        UID uid = uniqueIdCache.getOrCreateId(uuid);
+
+        Assertions.assertThat(uid).isNotNull();
+
+        String uuid2 = uniqueIdCache.getName(uid);
+
+        Assertions.assertThat(uuid).isEqualTo(uuid2);
+    }
 }
