2cc58eed8870134ee7d6b2701d43275a3e430ae3
at055612 <at055612@users.noreply.github.com>
2019-03-13 15:15:13 +0000

    Fix typo in Dockerfile

--- buildLocalDocker.sh
+++ buildLocalDocker.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+set -e
+
 #run a build with no tests for speed
 ./buildNoIntTests.sh
 
 #define the docker context as stroom-app/docker as this is where all the docker artefacts are, including the dockerfile
-docker build --tag=gchq/stroom-stats:v1.0.0-LOCAL-SNAPSHOT --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy ./docker/.
+docker build \
+  --tag=gchq/stroom-stats:v1.0.0-LOCAL-SNAPSHOT \
+  --build-arg http_proxy=$http_proxy \
+  --build-arg https_proxy=$https_proxy \
+  ./docker/stroom-stats
+
+docker build \
+  --tag=gchq/stroom-stats-hbase:v1.0.0-LOCAL-SNAPSHOT \
+  --build-arg http_proxy=$http_proxy \
+  --build-arg https_proxy=$https_proxy \
+  ./docker/stroom-stats-hbase
